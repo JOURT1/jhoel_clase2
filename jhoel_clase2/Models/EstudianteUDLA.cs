b13dc983cfc9f829be4f6e922d1bed2db6a7343f
JOURT1 <jhoelsuarez02@gmail.com>
2024-10-01 08:39:30 -0500

    Update 1

---
+++ jhoel_clase2/Models/EstudianteUDLA.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using System.ComponentModel.DataAnnotations;
+using System.Diagnostics.CodeAnalysis;
+
+namespace jhoel_clase2.Models
+{
+    [Authorize]//todos los metodos y atrivutos tienen authorize
+    public class EstudianteUDLA
+    {
+        [Required]// siempre va a ser requerido
+        public string IDBaner {  get; set; }
+
+        [AllowNull]//permite que el campo sea nulo  -- anotaciones
+        [EmailAddress]//valida que sea correo
+        public string? Correo { get; set; }
+
+        [MaxLength(50)]//longitud maxima de 50
+        public string Nombre { get; set; }
+
+        //public List<Carrera> Carreras { get; set; } listado de carreras
+        public Carrera Carrera { get; set; }
+    }
+}
